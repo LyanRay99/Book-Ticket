fc17167b75bec2954b95d89b4571c0fa4b09fd75
Tai Marce <mttai161099@gmail.com>
2023-03-18 13:05:04 +0700

    create fake data backup into seeder

---
+++ Backend/Middlewares/station.middleware.js
@@ -0,0 +1,17 @@
+const { Station } = require("../models/");
+
+const checkID = async (req, res, next) => {
+  const { id } = req.params;
+
+  const check = await Station.findOne({
+    where: {
+      id,
+    },
+  });
+
+  check ? next() : res.status(404).send(`not found id: ${id}`);
+};
+
+module.exports = {
+  checkID,
+};
--- Backend/Routers/station.router.js
+++ Backend/Routers/station.router.js
@@ -1,5 +1,9 @@
 const express = require("express");
+
+//* routers
 const stationRouter = express.Router();
+
+//* Controller
 const {
   C_createStation,
   C_getStation,
@@ -8,12 +12,15 @@ const {
   C_deleteStation,
 } = require("../Controllers/station.controller");
 
+//* Middleware
+const { checkID } = require("../Middlewares/station.middleware");
+
 const stationPath = "/stations";
 stationRouter.post(stationPath, C_createStation);
 stationRouter.get(stationPath, C_getStation);
-stationRouter.get(`${stationPath}/:id`, C_getStationDetail);
-stationRouter.put(`${stationPath}/:id`, C_updateStation);
-stationRouter.delete(`${stationPath}/:id`, C_deleteStation);
+stationRouter.get(`${stationPath}/:id`, checkID, C_getStationDetail);
+stationRouter.put(`${stationPath}/:id`, checkID, C_updateStation);
+stationRouter.delete(`${stationPath}/:id`, checkID, C_deleteStation);
 
 module.exports = {
   stationRouter,
