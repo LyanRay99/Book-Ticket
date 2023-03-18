3c718beade94dfde0f4cdfc4fecf9ff8a86caf03
Tai Marce <mttai161099@gmail.com>
2023-03-18 15:49:02 +0700

    feature login

--- Backend/Controllers/user.controller.js
+++ Backend/Controllers/user.controller.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt");
 //* import model Users
 const { Users } = require("../models");
 
+//* register
 const C_register = async (req, res) => {
   const { name, email, password, numberPhone } = req.body;
   try {
@@ -26,6 +27,29 @@ const C_register = async (req, res) => {
   }
 };
 
+//* login
+const C_login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await Users.findOne({
+      where: {
+        email,
+      },
+    });
+
+    //* check password
+    //* vì ta đã mã hóa password khi user register nên ta cần dùng bcrypt.compareSync() để ktr password có đúng ko?
+    const isAuthentication = bcrypt.compareSync(password, user.password);
+
+    isAuthentication
+      ? res.status(200).send({ message: "Successfully Login" })
+      : res.status(404).send({ message: "Incorrect Password" });
+  } catch (error) {
+    res.status(500).send({ message: "Not Found Email" });
+  }
+};
 module.exports = {
   C_register,
+  C_login,
 };
--- Backend/Routers/user.router.js
+++ Backend/Routers/user.router.js
@@ -4,13 +4,13 @@ const express = require("express");
 const userRouter = express.Router();
 
 //* Controller
-const { C_register } = require("../Controllers/user.controller");
+const { C_register, C_login } = require("../Controllers/user.controller");
 
 //* Middleware
 // const { checkID } = require("../Middlewares/Middleware");
 
 userRouter.post(`/register`, C_register);
-// stationRouter.get(stationPath, C_getStation);
+userRouter.post(`/login`, C_login);
 // stationRouter.get(`${stationPath}/:id`, checkID, C_getStationDetail);
 // stationRouter.put(`${stationPath}/:id`, checkID, C_updateStation);
 // stationRouter.delete(`${stationPath}/:id`, checkID, C_deleteStation);
