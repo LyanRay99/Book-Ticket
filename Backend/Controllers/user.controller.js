4f4292ac529dc68c26ef65221ef34f94388e5513
Tai Marce <mttai161099@gmail.com>
2023-03-19 21:26:52 +0700

    feature read, update, delete Users

--- Backend/Controllers/user.controller.js
+++ Backend/Controllers/user.controller.js
@@ -91,8 +91,72 @@ const C_uploadAvatar = async (req, res, next) => {
   });
 };
 
+const C_getUsers = async (req, res) => {
+  try {
+    const users = await Users.findAll();
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+const C_getUsersDetail = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await Users.findOne({
+      where: {
+        id,
+      },
+    });
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+const C_updateUsers = async (req, res) => {
+  const { id } = req.params;
+  const { name, password, numberPhone } = req.body;
+  try {
+    await Users.update(
+      {
+        name: name,
+        password: password,
+        numberPhone: numberPhone,
+      },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+
+    res.status(200).send(`updated User have id: ${id}`);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+const C_deleteUsers = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Users.destroy({
+      where: {
+        id,
+      },
+    });
+    res.status(201).send(`deleted User have id: ${id}`);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
 module.exports = {
   C_register,
   C_login,
   C_uploadAvatar,
+  C_getUsers,
+  C_getUsersDetail,
+  C_updateUsers,
+  C_deleteUsers,
 };
--- Backend/Routers/user.router.js
+++ Backend/Routers/user.router.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const multer = require("multer");
 
 //* model
-const { User } = require("../models");
+const { Users } = require("../models");
 
 //* routers
 const userRouter = express.Router();
@@ -12,13 +11,20 @@ const {
   C_register,
   C_login,
   C_uploadAvatar,
+  C_getUsers,
+  C_getUsersDetail,
+  C_updateUsers,
+  C_deleteUsers,
 } = require("../Controllers/user.controller");
 
 //* Middleware
-const { M_checkEmailExist } = require("../Middlewares/validations/validation");
+const {
+  M_checkEmailExist,
+  M_checkID,
+} = require("../Middlewares/validations/validation");
 const { M_uploadAvatar } = require("../Middlewares/Uploads/uploadImg");
 const { M_authentication } = require("../Middlewares/auth/authentication");
-// const { M_authorize } = require("../Middlewares/auth/authorize");
+const { M_authorize } = require("../Middlewares/auth/authorize");
 
 userRouter.post(`/register`, M_checkEmailExist, C_register);
 userRouter.post(`/login`, C_login);
@@ -27,6 +33,28 @@ userRouter.post(
   M_authentication,
   M_uploadAvatar("Avatar"),
   C_uploadAvatar
+);
+userRouter.get(`/`, M_authentication, M_authorize, C_getUsers);
+userRouter.get(
+  `/:id`,
+  M_authentication,
+  M_authorize,
+  M_checkID(Users),
+  C_getUsersDetail
+);
+userRouter.put(
+  `/:id`,
+  M_authentication,
+  M_authorize,
+  M_checkID(Users),
+  C_updateUsers
+);
+userRouter.delete(
+  `/:id`,
+  M_authentication,
+  M_authorize,
+  M_checkID(Users),
+  C_deleteUsers
 );
 
 module.exports = {
